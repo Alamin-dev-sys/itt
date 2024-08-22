82f28ddb0e3c88468dd9402450519b52be6f3f1c
Alamin-dev-sys <alamin.dev.sys@gmail.com>
2024-08-22 15:10:28 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+     fontFamily:{
+      'custom':['Anek Devanagari',' sans-serif']
+     }
     },
   },
   plugins: [],
 };
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
